9204825f538178b800289839698a227929c02002
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-07-26 18:31:03 +0530

    Deploy test

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,11 +35,12 @@ export default function Example() {
                   >
                     <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                       <div className="flex items-center justify-between w-full md:w-auto">
-                        <a href="#">
+                        <a href="/">
                           <span className="sr-only">Workflow</span>
                           <img
                             className="h-8 w-auto sm:h-10"
                             src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                            alt=""
                           />
                         </a>
                         <div className="-mr-2 flex items-center md:hidden">
@@ -64,7 +65,7 @@ export default function Example() {
                         </a>
                       ))}
                       <a
-                        href="#"
+                        href="/"
                         className="font-medium text-indigo-600 hover:text-indigo-500"
                       >
                         Log in
@@ -119,7 +120,7 @@ export default function Example() {
                         ))}
                       </div>
                       <a
-                        href="#"
+                        href="/"
                         className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
                       >
                         Log in
@@ -147,7 +148,7 @@ export default function Example() {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="#"
+                    href="/"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
                     Get started
@@ -155,7 +156,7 @@ export default function Example() {
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
-                    href="#"
+                    href="/"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
                     Live demo
