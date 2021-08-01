e2aac76e1e6c10a811c09fdf04a4a3941f871b12
Salik <salik.ansari6@gmail.com>
2021-08-01 16:45:27 +0530

    :adhesive_bandage: Fixed merge conflict

--- src/components/DashboardGrid.js
+++ src/components/DashboardGrid.js
@@ -1,10 +1,13 @@
 import React from "react";
 import SplitBillReq from "./SplitBillReq";
+import SplitWith from "./SplitWith";
 
 const DashboardGrid = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center p-2">
-      <div className="bg-red-400 h-24 order-1">1</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 p-4 lg:grid-cols-3 place-items-center">
+      <div className="order-1 p-2">
+        <SplitWith />
+      </div>
       <div className="bg-blue-400 order-4">2</div>
       <div className="bg-red-400 h-24 order-2">3</div>
       <div className="bg-blue-400 order-5">4</div>
---
+++ src/components/SplitWith.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+const SplitWith = () => {
+  return (
+    <div className="w-96 p-3 rounded-3xl bg-gradient-to-br from-red-400 to-red-600">
+      <div className="bg-white rounded-3xl p-3 flex flex-col">
+        <img
+          src="/images/pizza.png"
+          alt="vendor"
+          className="h-56 filter drop-shadow-xl mx-auto"
+        />
+
+        <div className="flex justify-around mt-5">
+          <div className="vendor-brand flex items-start">
+            <img
+              src="/images/pizza.png"
+              alt=""
+              className="vendor-logo h-5 mr-2"
+            />
+            <span className="vendor-name">Pizza Inn</span>
+          </div>
+          <div className="total-bill">
+            <div className="text-gray-500">Total Bill</div>
+            <div className="font-bold text-xl text-gray-700">$360.80</div>
+          </div>
+        </div>
+        <button className="mx-auto text-white bg-gradient-to-br from-blue-400 to-blue-800 py-3 px-5 rounded-lg mt-2">
+          Split Now
+        </button>
+      </div>
+      <div className="split-with p-3">
+        <span className="font-bold text-white text-xl">Split with</span>
+        <div className="avatar-list flex mt-5">
+          <img
+            className="h-12 w-12 rounded-full  border-white border-2 -mr-3 shadow"
+            src="https://i.pravatar.cc/300?img=1"
+            alt=""
+          />
+          <img
+            className="h-12 w-12 rounded-full  border-white border-2 -mr-3 shadow"
+            src="https://i.pravatar.cc/300?img=2"
+            alt=""
+          />
+          <img
+            className="h-12 w-12 rounded-full  border-white border-2 -mr-3 shadow"
+            src="https://i.pravatar.cc/300?img=3"
+            alt=""
+          />
+          <img
+            className="h-12 w-12 rounded-full  border-white border-2 -mr-3 shadow"
+            src="https://i.pravatar.cc/300?img=4"
+            alt=""
+          />
+          <img
+            className="h-12 w-12 rounded-full  border-white border-2 -mr-3 shadow"
+            src="https://i.pravatar.cc/300?img=5"
+            alt=""
+          />
+          <img
+            className="h-12 w-12 rounded-full  border-white border-2 -mr-3 shadow"
+            src="https://i.pravatar.cc/300?img=6"
+            alt=""
+          />
+          <img
+            className="h-12 w-12 rounded-full  border-white border-2 -mr-3 shadow"
+            src="https://i.pravatar.cc/300?img=7"
+            alt=""
+          />
+          <img
+            className="h-12 w-12 rounded-full  border-white border-2 -mr-3 shadow"
+            src="https://i.pravatar.cc/300?img=8"
+            alt=""
+          />
+          <img
+            className="h-12 w-12 rounded-full  border-white border-2 -mr-3 shadow"
+            src="https://i.pravatar.cc/300?img=9"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SplitWith;
