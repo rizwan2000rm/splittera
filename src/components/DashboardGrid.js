28d2f206901f89c23e45277243034218c931d999
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-08-01 15:58:09 +0530

    UI: Split Bill Request Component

--- src/components/DashboardGrid.js
+++ src/components/DashboardGrid.js
@@ -1,13 +1,16 @@
 import React from "react";
+import SplitBill from "./SplitBill";
 
 const DashboardGrid = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center p-2">
       <div className="bg-red-400 h-24 order-1">1</div>
       <div className="bg-blue-400 order-4">2</div>
       <div className="bg-red-400 h-24 order-2">3</div>
       <div className="bg-blue-400 order-5">4</div>
-      <div className="bg-red-400 h-24 order-3">5</div>
+      <div className="order-3 w-full h-full p-2">
+        <SplitBill />
+      </div>
       <div className="bg-blue-400 order-6">6</div>
     </div>
   );
---
+++ src/components/SplitBill.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const SplitBill = () => {
+  return (
+    <div className="border max-w-sm h-full rounded-lg px-2 py-8 mx-auto">
+      <img
+        className="w-32 h-32 object-cover rounded-full mx-auto p-1 border-2 border-yellow-400"
+        src="https://images.unsplash.com/photo-1552234994-66ba234fd567?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80"
+        alt=""
+      />
+      <h1 className="font-bold text-base text-center pt-6">Hey Mike!</h1>
+      <p className="text-center mx-auto px-8 text-sm pt-2">
+        <span className="font-bold">Rakib</span> requested you a split bill
+        payment of <span className="font-medium text-yellow-400">$20.00</span>{" "}
+        from <span className="font-bold">Sushi Restaurant</span>
+      </p>
+      <div className="px-8 pt-8 flex flex-col gap-2">
+        <div className="flex justify-between">
+          <div className="text-base">Split Bill</div>
+          <div className="font-bold text-gray-600">$20.00</div>
+        </div>
+        <div className="flex justify-between">
+          <div className="text-base">Total Bill</div>
+          <div className="font-bold text-gray-600">$220.00</div>
+        </div>
+        <div className="flex justify-between">
+          <div className="text-base">Details</div>
+          <div className="font-bold text-red-400">Items</div>
+        </div>
+      </div>
+      <div className="px-8 pt-4 flex gap-4">
+        <button className="px-6 py-3 w-1/2 rounded-lg bg-red-400 text-white text-sm hover:opacity-80">
+          Decline
+        </button>
+        <button className="px-6 py-3 w-1/2 rounded-lg bg-blue-400 text-white text-sm hover:opacity-80">
+          Accept
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SplitBill;
