3fafee00b24d967198b9e4da7ab5ddb890636186
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-08-08 15:48:09 +0530

    Feat: Redux toolkit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,34 @@
 import React from "react";
 import Header from "../components/Header";
 import Feature from "../components/Feature";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../features/counter/counterSlice";
 
 const Home = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <Header />
       <Feature />
+      {/* <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div> */}
     </div>
   );
 };
