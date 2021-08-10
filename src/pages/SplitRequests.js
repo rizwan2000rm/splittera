0d178bf3c7fd0c539cc6995ae3ba6303f140f7ba
Salik <salik.ansari6@gmail.com>
2021-08-10 22:37:31 +0530

    :lipstick: UI: Added message if there is no history or pending requests

--- src/pages/SplitRequests.js
+++ src/pages/SplitRequests.js
@@ -42,26 +42,30 @@ const SplitRequests = () => {
   return (
     <div className="requests flex m-6">
       <div className="req-list w-72 flex flex-col border border-gray-300">
-        {requests?.map((request) => (
-          <div
-            className="req-list-item flex gap-4 items-center pl-4 py-2 border-b border-gray-300 cursor-pointer"
-            onClick={() => {
-              setActiveRequest(request);
-            }}
-          >
-            <img
-              src="https://as2.ftcdn.net/v2/jpg/02/88/84/21/1000_F_288842168_nhgjfMO0vtARTs6obR3i9bfdRIuaSL56.jpg"
-              className="h-12 w-12"
-              alt=""
-            />
-            <div className="req-details p-2">
-              <div className="name text-gray-600 text-xl font-bold mb-2">
-                {request.name}
+        {requests.length !== 0 ? (
+          requests.map((request) => (
+            <div
+              className="req-list-item flex gap-4 items-center pl-4 py-2 border-b border-gray-300 cursor-pointer"
+              onClick={() => {
+                setActiveRequest(request);
+              }}
+            >
+              <img
+                src="https://as2.ftcdn.net/v2/jpg/02/88/84/21/1000_F_288842168_nhgjfMO0vtARTs6obR3i9bfdRIuaSL56.jpg"
+                className="h-12 w-12"
+                alt=""
+              />
+              <div className="req-details p-2">
+                <div className="name text-gray-600 text-xl font-bold mb-2">
+                  {request.name}
+                </div>
+                <div className="date text-sm">12th January 2021</div>
               </div>
-              <div className="date text-sm">12th January 2021</div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <h1 className="font-medium text-xl m-5">No pending requests</h1>
+        )}
       </div>
       <div className="active-split ">
         {activeRequest && <ActiveSplitRequest activeRequest={activeRequest} />}
