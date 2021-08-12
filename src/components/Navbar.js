f30372da3b90e67dc53ca3b0d4bd7e1172f107b1
Salik <salik.ansari6@gmail.com>
2021-08-12 14:32:32 +0530

    :lipstick: UI: modal closed when form submits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,10 +44,11 @@ const Navbar = () => {
   //   }
   // };
 
-  const handleSubmit = () => {
+  const handleSubmit = (close) => {
     const userEmails = value.map((email) => email.value);
     addBill(vendor, amount, userEmails, authUser)
       .then(() => {
+        close();
         setVendor("");
         setValue([]);
         setAmount("");
@@ -110,7 +111,6 @@ const Navbar = () => {
   return (
     <>
       <div className="flex justify-between md:justify-start md:flex-col items-center w-full h-full py-4 shadow-xl">
-        =======
         <img
           className="w-1/3 mx-auto mb-3 lg:mb-6"
           src="/images/logo.svg"
@@ -181,7 +181,9 @@ const Navbar = () => {
                   <button
                     type="submit"
                     className="px-6 py-2 w-full mx-auto rounded-lg bg-blue-400 text-white text-sm hover:opacity-80"
-                    onClick={handleSubmit}
+                    onClick={() => {
+                      handleSubmit(close);
+                    }}
                   >
                     Split Now
                   </button>
@@ -207,6 +209,17 @@ const Navbar = () => {
           <span className="hidden lg:block">History</span>
         </NavLink>
       </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 };
