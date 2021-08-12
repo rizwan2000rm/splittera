3a2885f1440fa8745a163fc7ee47d77c7b4787d6
Salik <salik.ansari6@gmail.com>
2021-08-12 12:44:58 +0530

    :lipstick: UI: Added redirect page if page content is loading

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,9 +1,14 @@
 import React, { useContext } from "react";
 import { Redirect, Route } from "react-router";
 import AuthUserContext from "../context/AuthUserContext";
+import Redirecting from "../pages/Redirecting";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const { isLoading, authUser } = useContext(AuthUserContext);
+
+  if (isLoading) {
+    return <Redirecting />;
+  }
 
   return (
     <Route
---
+++ src/pages/Redirecting.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Redirecting = () => {
+  return (
+    <div className="h-screen text-2xl font-medium w-screen flex justify-center items-center">
+      Redirecting....
+    </div>
+  );
+};
+
+export default Redirecting;
